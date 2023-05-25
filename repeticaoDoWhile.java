bbebe98cf974b6767279b32215d36d601cdf66bf
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2023-05-24 23:20:23 -0300

    Add files via upload

---
+++ repeticaoDoWhile.java
@@ -0,0 +1,19 @@
+package estruturasCondicionais;
+
+public class repeticaoDoWhile {
+
+	public static void main(String[] args) {
+		
+		int valor = 1;
+		
+        do
+        {
+        	System.out.printf("%d ",valor);
+        	++valor;
+        }while(valor<= 20);//fim da instrução do ... while
+		
+        System.out.println();	
+        	
+	}
+
+}
